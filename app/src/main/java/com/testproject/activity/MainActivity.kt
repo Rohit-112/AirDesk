3c0a73fa5d4a63eea9f40732f9f99ec4b460c9ed
Rohit Sharma <rohitksharma345@gmail.com>
2025-11-18 16:57:04 +0530

    complete setup upto navigation between screen

--- app/src/main/java/com/testproject/activity/MainActivity.kt
+++ app/src/main/java/com/testproject/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.testproject.activity
 
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.testproject.R
 import com.testproject.base.BaseActivity
 import com.testproject.databinding.ActivityMainBinding
 import com.testproject.utils.show
@@ -12,16 +15,24 @@ class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupNavigation()
     }
 
     fun btmNavShow(isShown: Boolean = true) {
         binding.bottomNav.show(isShown)
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/testproject/fragment/HomeFragment.kt
+++ app/src/main/java/com/testproject/fragment/HomeFragment.kt
@@ -1,10 +1,29 @@
 package com.testproject.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.testproject.base.BaseFragment
+import com.testproject.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment: BaseFragment() {
+class HomeFragment : BaseFragment() {
 
+    lateinit var binding: FragmentHomeBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btmNavShow(true)
+    }
 }
